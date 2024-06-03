378e13a27b610c1049544426415e737cd9fd9bc9
FaisalMahi <faisalibnaziz39@gmail.com>
2024-06-04 01:10:12 +0600

    changed file

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:personal_finance_tracker/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Finance Tracker',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Personal Finance Tracker'),
+    );
+  }
+}
